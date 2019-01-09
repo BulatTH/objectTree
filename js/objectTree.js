f2390f7933d30a727099b27a46e113ddc73b6d9e
Bulat Khairutdinov <bulatxajrutdinoff@yandex.ru>
2019-01-09 10:42:15 +0300

    .

--- js/objectTree.js
+++ js/objectTree.js
@@ -2,8 +2,6 @@ function createTree(container, data) {
     let tmp = data,
         ul = document.createElement("ul");
 
-    
-
     for (let val in tmp) {
        let li = document.createElement("li"), 
            text = (Object.keys(tmp[val]).length != 0) ? val + ` [${Object.keys(tmp[val]).length}]` : val;
